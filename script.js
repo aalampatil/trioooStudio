36bf201b5ccd685fd472cab12f60fcd48ae111ec
Aalam@DESKTOP-I20EQ8S <aalampatil001@gmail.com>
2024-10-28 14:48:25 +0530

    final design

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleFeatures(id) {
-    const featuresDiv = document.getElementById(id);
-    if (featuresDiv.style.display === "none" || featuresDiv.style.display === "") {
-        featuresDiv.style.display = "block"; // Show features
-    } else {
-        featuresDiv.style.display = "none"; // Hide features
-    }
-}
